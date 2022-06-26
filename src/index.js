9d08e6afa0969295d7497ed718aca09674b3d2b7
uxknow <secretnerowww@gmail.com>
2022-06-26 14:45:20 +0300

    feat: complete task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let result ='';
+  let abs = Math.abs(n);
+  let str = abs.toString();
+  for (let i=0; i < str.length; i += 1) {
+    result = `${str[i]}${result}`;
+  }
+  return +result;
 }
+
+
